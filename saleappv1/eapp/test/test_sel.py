e4beeaa5fb919bac8e89d57fc4fc37efb6cc9d57
ITPhongou23 <nt.phong11405@gmail.com>
2026-05-15 15:51:11 +0700

    Update: thực hành ngày 15/5/2026.

--- saleappv1/eapp/test/test_sel.py
+++ saleappv1/eapp/test/test_sel.py
@@ -1,6 +1,11 @@
+import pytest
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
+from eapp.test.pages.CartPage import CartPage
 from eapp.test.pages.HomePage import HomePage
+from eapp.test.pages.LoginPage import LoginPage
+from eapp.test.pages.RegisterPage import RegisterPage
 from eapp.test.test_base import driver, test_app
 import time
 
@@ -26,4 +31,90 @@ def test_order(driver):
 
     e = driver.find_element(By.CLASS_NAME, 'cart-counter')
     assert int(e.text) == 3
+
+def test_login_success(driver):
+    login = LoginPage(driver=driver)
+    login.open_page()
+    login.login(username='admin',password='123456')
+
+    time.sleep(1)
+
+    assert driver.current_url == 'http://127.0.0.1:5000/'
+
+    e = driver.find_element(By.CSS_SELECTOR,'#collapsibleNavbar > ul > li:nth-child(5) > a')
+    assert  (f'Chào admin' in e.text)
+
+def test_login_redirect_success(driver):
+    login = LoginPage(driver=driver)
+    login.open_page('http://127.0.0.1:5000/login?next=/cart')
+    login.login(username='admin',password='123456')
+
+    time.sleep(1)
+
+    assert driver.current_url == 'http://127.0.0.1:5000/cart'
+
+    e = driver.find_element(By.CSS_SELECTOR,'#collapsibleNavbar > ul > li:nth-child(5) > a')
+    assert  (f'Chào admin' in e.text)
+
+def test_pay_success(driver):
+    home = HomePage(driver=driver)
+    home.open_page()
+    home.order()
+
+    time.sleep(1)
+
+    login = LoginPage(driver=driver)
+    login.open_page()
+    login.login(username='admin',password='123456')
+
+    time.sleep(1)
+
+    cart = CartPage(driver=driver)
+    cart.open_page()
+    cart.pay()
+
+    time.sleep(1)
+
+    with pytest.raises(NoSuchElementException):
+        driver.find_element(By.TAG_NAME,'table')
+
+def test_udpate_cart(driver):
+    home = HomePage(driver=driver)
+    home.open_page()
+    home.order()
+
+    time.sleep(1)
+
+    login = LoginPage(driver=driver)
+    login.open_page()
+    login.login(username='admin',password='123456')
+
+    time.sleep(1)
+
+    cart = CartPage(driver=driver)
+    cart.open_page()
+    cart.update_cart_item(3)
+
+    driver.implicitly_wait(3)
+
+    e = driver.find_element(By.CLASS_NAME,'cart-counter')
+    assert int(e.text) == 4
+
+def test_register(driver):
+    register = RegisterPage(driver=driver)
+    register.open_page()
+    register.register(name='demo',username='demodemo',password='Demo@123',confirm='Demo@123',avatar='D:\Capture.PNG')
+
+    login = LoginPage(driver=driver)
+    login.open_page()
+    login.login(username='demodemo',password='Demo@123')
+
+    time.sleep(1)
+
+    assert driver.current_url == 'http://127.0.0.1:5000/'
+
+    e = driver.find_element(By.CSS_SELECTOR,'#collapsibleNavbar > ul > li:nth-child(5) > a')
+    assert  (f'Chào demodemo' in e.text)
+
+
 
